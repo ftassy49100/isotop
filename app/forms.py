77b1ed62ea351a9845af2cd81215966b95fad949
root <root@python>
2017-02-17 07:29:10 -0500

    modify base.html

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,14 @@
 from flask_wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import StringField, BooleanField, SelectField
 from wtforms.validators import DataRequired
 from wtforms.ext.sqlalchemy.orm import model_form
-from app.models import qualityFolder
+
+from app import db, models
+
 
 class LoginForm(Form):
 	nickname = StringField('nickname', validators=[DataRequired()])
 	remember_me = BooleanField('remember_me', default=False)
 
-CreateFolderForm = model_form(qualityFolder.QualityFolder, Form)
+class QualityFolderCreationForm(Form):
+	opened_by = SelectField(u'Opened by', choices=)
